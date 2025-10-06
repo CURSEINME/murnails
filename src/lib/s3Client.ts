509a877377a7b2c8380a5073424c44d6a82694ce
CURSEINME <brff282@gmail.com>
2025-10-06 09:19:53 +0300

    add s3Client

---
+++ src/lib/s3Client.ts
@@ -0,0 +1,28 @@
+import { S3Client } from '@aws-sdk/client-s3';
+
+const REGION = process.env.REGION;
+const AWS_KEY_ID = process.env.AWS_KEY_ID;
+const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+const ENDPOINT = process.env.ENDPOINT;
+
+if (!REGION) {
+  throw new Error('REGION environment variable is not set.');
+}
+
+if (!AWS_KEY_ID) {
+  throw new Error('AWS_KEY_ID environment variable is not set.');
+}
+
+if (!AWS_SECRET_KEY) {
+  throw new Error('AWS_SECRET_KEY environment variable is not set.');
+}
+
+if (!ENDPOINT) {
+  throw new Error('ENDPOINT environment variable is not set.');
+}
+
+export const aws = new S3Client({
+  credentials: { accessKeyId: AWS_KEY_ID, secretAccessKey: AWS_SECRET_KEY },
+  region: REGION,
+  endpoint: ENDPOINT,
+});
