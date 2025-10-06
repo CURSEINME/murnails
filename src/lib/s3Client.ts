a7ab3839de59609de35964f95dbda3cd85ccfa54
CURSEINME <brff282@gmail.com>
2025-10-06 14:39:27 +0300

    rename env var

--- src/lib/s3Client.ts
+++ src/lib/s3Client.ts
@@ -2,7 +2,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 
 export const REGION = process.env.REGION;
 export const AWS_KEY_ID = process.env.AWS_KEY_ID;
-export const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+export const S3_SECRET_KEY = process.env.S3_SECRET_KEY;
 export const ENDPOINT = process.env.ENDPOINT;
 export const BUCKET = process.env.BUCKET_NAME;
 
@@ -14,8 +14,8 @@ if (!AWS_KEY_ID) {
   throw new Error('AWS_KEY_ID environment variable is not set.');
 }
 
-if (!AWS_SECRET_KEY) {
-  throw new Error('AWS_SECRET_KEY environment variable is not set.');
+if (!S3_SECRET_KEY) {
+  throw new Error('S3_SECRET_KEY environment variable is not set.');
 }
 
 if (!ENDPOINT) {
@@ -23,7 +23,7 @@ if (!ENDPOINT) {
 }
 
 export const aws = new S3Client({
-  credentials: { accessKeyId: AWS_KEY_ID, secretAccessKey: AWS_SECRET_KEY },
+  credentials: { accessKeyId: AWS_KEY_ID, secretAccessKey: S3_SECRET_KEY },
   region: REGION,
   endpoint: ENDPOINT,
 });
