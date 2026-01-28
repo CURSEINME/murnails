4c0d71958e7e8299e84095c7eb3f945b1c988714
CURSEINME <brff282@gmail.com>
2026-01-28 10:19:11 +0300

    add margin to footer

--- src/app/components/UI/Footer.tsx
+++ src/app/components/UI/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
 	return (
-		<footer className='mt-auto text-sm md:text-md text-white bg-black/50 border-t border-white/10 flex flex-col items-center py-6 gap-2 md:gap-4'>
+		<footer className='mt-20 text-sm md:text-md text-white bg-black/50 border-t border-white/10 flex flex-col items-center py-6 gap-2 md:gap-4'>
 			<div className='flex justify-center gap-4 md:gap-8'>
 				<Link href='/#about-us'>О нас</Link>
 				<Link href='/#gallery'>Работы</Link>
@@ -10,7 +10,6 @@ export default function Footer() {
 				<Link href='/#contacts'>Контакты</Link>
 			</div>
 			<div className='text-white/70 text-xs md:text-sm'>© 2026 Murnails. Все права защищены.</div>
-
 		</footer>
 	)
 }
