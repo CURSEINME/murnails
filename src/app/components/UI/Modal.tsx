c6b0258af697d77c7a51a9b1bf834ffa603e34f6
CURSEINME <brff282@gmail.com>
2026-01-28 10:17:13 +0300

    fix modal

--- src/app/components/UI/Modal.tsx
+++ src/app/components/UI/Modal.tsx
@@ -10,7 +10,16 @@ export default function Modal({ children, onClose, overlayClassName }: ModalProp
       onClick={onClose}
       className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 px-2 md:px-5"
     >
-      <div className={overlayClassName} onClick={(e) => e.stopPropagation()}>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={`
+          w-full
+          max-w-lg
+          max-h-[90dvh]
+          overflow-y-auto
+          ${overlayClassName ?? ''}
+        `}
+      >
         {children}
       </div>
     </div>
